4038e9d3f33a994d8cc3fe6ae793d5d8763b367c
70xH <70xH@tutanota.com>
2021-01-18 16:51:17 +0000

    extra small tests

--- src/exp-text.cpp
+++ src/exp-text.cpp
@@ -6,5 +6,10 @@
 int main()
 {
   print(define_int(42));
+  print(define_char("\n"));
+  print(define_char("c"));
+  print(define_char("\n"));
+  print(assignPairs(define_int(10), define_string("string")));
+  print(define_char("\n"));
   return 0;
 }
